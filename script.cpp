cda3608dd718f02567b65e97b78423cde2f99395
Nahida <51387274+NahidaMika@users.noreply.github.com>
2025-09-30 04:00:09 +0000

    Other

--- script.cpp
+++ script.cpp
@@ -15,7 +15,7 @@ int main() {
     //Constants
     const int fj = 12;
 
-    //Structures
+    //Structures --> way to group several related variables into one place.
     struct {
         // int --> stores integers (whole numbers), without decimals, such as 123 or -123
         int ab = -100;
@@ -27,16 +27,39 @@ int main() {
         char de = 'H';
         // Bool -> Boolean --> true (1) or false (0)
         bool ef = true;
-    };
+        // Reference --> reference variable is an alias for an existing variable. It is created using the & operator
+        int &fg = ab;
+        // Pointer --> stores the memory address as its value.
+        int* ptr = &ab;
+        /*
+            string* mystring; // Preferred
+            string *mystring;
+s           tring * mystring;
+        */
 
-    //Enums
+    } Info;
+    
+    std::cout << Info.ab << std::endl;
+    std::cout << Info.fg << std::endl;
+    std::cout << Info.ptr << std::endl;
+    
+
+    //Enums --> special type that represents a group of constants
     enum Level {
-    LOW,
-    MEDIUM = 10,
+    LOW = 6,
+    MEDIUM,
     HIGH
     };
 
     enum Level lvl = LOW;
+    enum Level lvl1 = MEDIUM;
+    enum Level lvl2 = HIGH;
+    std::cout << lvl << std::endl;
+    std::cout << lvl1 << std::endl;
+    std::cout << lvl2 << std::endl;
+
+    std::cout << "Bytes used by a[1]: ";
+    std::cout << sizeof(a[1]) << std::endl;
     
 
 };
