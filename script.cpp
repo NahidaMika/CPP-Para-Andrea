1128b030e68f6602c648452ad4dec23a8273873a
Nahida <51387274+NahidaMika@users.noreply.github.com>
2025-09-30 03:24:41 +0000

    Explaining

--- script.cpp
+++ script.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
+/* Multi line
+    Comment
+*/
+
 int main() {
-    int pointerw = 10;
-    double value;
-    int* pointer =  &pointerw;
-    std::cout << "Enter value:" << std::endl;
-    std::cin >> value;
-    std::cout << pointer;
-    std::cout << sqrt(value+pointerw);
-}
+    //Array x[n]
+    double a[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+    //Constants
+    const int fj = 12;
+
+    //Structures
+    struct {
+        // int --> stores integers (whole numbers), without decimals, such as 123 or -123
+        int ab = -100;
+        // Strings --> stores text, such as "Hello World". String values are surrounded by double quotes
+        string bc = "Hello World";
+        // Double--> stores floating point numbers, with decimals, such as 19.99 or -19.99
+        double cd = 1.999;
+        // Char --> stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+        char de = 'H';
+        // Bool -> Boolean --> true (1) or false (0)
+        bool ef = true;
+    };
+
+    //Enums
+    enum Level {
+    LOW,
+    MEDIUM = 10,
+    HIGH
+    };
+
+    enum Level lvl = LOW;
+    
+
+};
