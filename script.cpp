d2d77c9e1470667761098e1676f2a7160773f7ef
Nahida <51387274+NahidaMika@users.noreply.github.com>
2025-09-30 00:35:39 +0000

    Prueba 1

---
+++ script.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int pointerw = 10;
+    double value;
+    int* pointer =  &pointerw;
+    std::cout << "Enter value:" << std::endl;
+    std::cin >> value;
+}
